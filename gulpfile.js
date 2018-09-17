75358aea41bb392bcc7ffb6d80d99769fd607fd3
Seb Haigh <seb.haigh@fuelmatrix.co.uk>
2018-09-17 21:06:13 +0100

    Updated gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+
+gulp.task('default', function() {
+    console.log("Hooray - you created a Gulp task");
+});
+
+gulp.task('html', function() {
+    console.log("Imagine something useful being done to your HTML here!");
+});
+
+gulp.task('watch', function() {
+
+    watch('.app/index.html', function() {
+        gulp.start('html');
+    });
+    
+});
